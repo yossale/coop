6764756a42c722bc38948ddeab687ded58023346
Eyal L <elpd.dev@runbox.com>
2015-01-14 16:36:44 +0200

    done.

--- source/public/js/weekly_report.js
+++ source/public/js/weekly_report.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$(".orders").hide();
-	
+
 	var opened = new Array();
-		
+
 	$(".product").click(function() {
 		var id = $(this).attr("id");
 		var orders = $(".orders[id=" + id + "]");
-		
+
 		if (opened[id] == true)
 		{
 			orders.hide();
@@ -17,11 +17,24 @@ $(document).ready(function() {
 			orders.show();
 			opened[id] = true;
 		}
-		
+
 	});
-	
-/*	
+
+/*
 	$(".date").datePicker({
 		startDate: '01/01/1990'
 	});*/
+
+	var $stockItemsForm = $("#stockItemsForm");
+	var $messages = $("#messages");
+
+	$stockItemsForm.areYouSure();
+	$stockItemsForm.bind('dirty.areYouSure', function() {
+		$messages.text("ישנם שינויים לא שמורים בטופס.");
+		$messages.toggleClass('messages-warning', true);
+	});
+	$stockItemsForm.bind('clean.areYouSure', function() {
+		$messages.text("");
+		$messages.toggleClass('messages-warning', false);
+	});
 });
